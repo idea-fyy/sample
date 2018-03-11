8bf8f9af93ead08ee8a0450f04d8e9c383ca8e39
root <root@homestead>
2018-03-11 15:34:49 +0000

    Finish login and logout

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Http\Requests;
 use App\Models\User;
+use Auth;
+
 
 class UsersController extends Controller
 {
@@ -32,6 +34,7 @@ class UsersController extends Controller
     		'password' => bcrypt($request->password),
     	]);
 
+        Auth::login($user);
     	session()->flash('success','欢迎！');
     	return redirect()->route('users.show', [$user]);
     }
